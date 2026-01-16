4b023785f46400dc6a07264d32a468531b0b78ad
Oumaima Assarti <ton-oumaima.assarti@student.vinci.be>
2026-01-16 17:24:49 +0100

    EMail validatoir wtest 3 and 4

--- src/emailValidator.test.ts
+++ src/emailValidator.test.ts
@@ -8,5 +8,13 @@ describe('Email Validator - IPL Social', () => {
   test('devrait rejeter un email avec plusieurs @', () => {
     expect(validateEmail('test@@example.com')).toBe(false);
   });
-  
+
+   test('devrait accepter un email avec un seul @', () => {
+    expect(validateEmail('test@example.com')).toBe(true);
+  });
+
+  test('devrait rejeter un email sans point dans le domaine', () => {
+    expect(validateEmail('test@example')).toBe(false);
+  });
+
 });
