8bf4197bde5eee46ac1ce4047fe2c974639fe70e
Oumaima Assarti <ton-oumaima.assarti@student.vinci.be>
2026-01-16 17:19:04 +0100

    Email validator working with 2 test passing

--- src/emailValidator.test.ts
+++ src/emailValidator.test.ts
@@ -4,4 +4,9 @@ describe('Email Validator - IPL Social', () => {
   test('devrait rejeter un email sans @', () => {
     expect(validateEmail('testexample.com')).toBe(false);
   });
+  
+  test('devrait rejeter un email avec plusieurs @', () => {
+    expect(validateEmail('test@@example.com')).toBe(false);
+  });
+  
 });
